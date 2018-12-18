cb8ccfea1c8feb6434cbfbce12bed0c42f8237f8
aaristov <andrey.aristov@pasteur.fr>
2018-12-18 13:57:23 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='pyhist',
+    version='0.1.0',
+    packages=['pyhist'],
+    install_requires=[
+        'scikit-image',
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'read-roi',
+        'pandas'
+    ],
+    url='https://github.com/aaristov/py-hist.git',
+    license='MIT',
+    author='Andrey Aristov',
+    author_email='aaristov@pasteur.fr',
+    description='Plot localization histograms'
+)
