e33df650d45f55929437b29f54124db45acb924f
aaristov <andrey.aristov@pasteur.fr>
2019-02-27 11:19:12 +0100

    minor improvements

--- pyhist/render.py
+++ pyhist/render.py
@@ -14,17 +14,36 @@ class IJroi:
         self.h = specs['height']
         
     def __repr__(self):
-        print(self.roi)
+        return f"roi.x={self.x}, roi.y={self.y}, roi.h={self.h}, roi.w={self.w}"
         
 
-def show_crop_xy(hist, xy:tuple=(None,None), size=None, vmax=None, zx=False, zy=False):
+def show_crop_xy(hist, xy:tuple=(None,None), size=None, vmax=None, zx=False, zy=False, show=False):
     z_proj = hist.max(axis=0)
     if xy[0] and xy[1] and size:
         z_proj = z_proj[xy[1]-size//2: xy[1]+size//2, xy[0]-size//2: xy[0]+size//2]
-    plt.imshow(z_proj, 
+    if show:
+        plt.imshow(z_proj, 
                vmax=vmax, 
                cmap='hot')
-    plt.show()
+        plt.show()
+
+def show_crop_xz(hist, xz:tuple=(None,None), size=None, vmax=None, zx=False, zy=False, show=False):
+    y_proj = hist.max(axis=1)
+    if xz[0] and xz[1] and size:
+        y_proj = y_proj[xz[1]-size//2: xz[1]+size//2, xz[0]-size//2: xz[0]+size//2]
+    plt.imshow(y_proj, 
+               vmax=vmax, 
+               cmap='hot')
+    if show: plt.show()
+
+def show_crop_yz(hist, yz:tuple=(None,None), size=None, vmax=None, zx=False, zy=False, show=False):
+    x_proj = hist.max(axis=2)
+    if yz[0] and yz[1] and size:
+        x_proj = x_proj[yz[1]-size//2: yz[1]+size//2, yz[0]-size//2: yz[0]+size//2]
+    plt.imshow(x_proj.T, 
+               vmax=vmax, 
+               cmap='hot')
+    if show: plt.show()
 
 def show_crop_xyz(hist, yx:tuple=None, crop=20, vmax=None, size=(5,5), origin='center'):
     if origin=='center': a, b = crop//2, crop//2
@@ -41,8 +60,15 @@ def show_crop_xyz(hist, yx:tuple=None, crop=20, vmax=None, size=(5,5), origin='c
     render_xyz(xy_proj, zy_proj, zx_proj, vmax, size)
     
 
-def show_crop_roi(hist:np.ndarray, roi:IJroi, z_lim=0, vmax=None, size=(10,3)):
-    xy_crop = hist[:, roi.y:roi.y+roi.h, roi.x:roi.x+roi.w] 
+def show_crop_roi(hist:np.ndarray, roi:IJroi, z_lim=0, vmax=None, size=(3,3), title=None):
+    print('hist shape: ', hist.shape)
+    print('roi: ', roi)
+    try:
+        xy_crop = hist[:, roi.y:roi.y+roi.h, roi.x:roi.x+roi.w] 
+    except TypeError as e:
+        print('hist shape: ', hist.shape)
+        print(roi)
+        raise e
     xy_proj = xy_crop.max(axis=0)
     if z_lim:
         z_max = np.argmax(xy_crop.max(axis=(1,2)))
@@ -51,7 +77,7 @@ def show_crop_roi(hist:np.ndarray, roi:IJroi, z_lim=0, vmax=None, size=(10,3)):
         zyx_proj = xy_crop
     zx_proj = zyx_proj.max(axis=1)
     #zy_proj = zyx_proj.max(axis=2)
-    render_xy_xz(xy_proj, zx_proj, vmax, size)
+    render_xy_xz(xy_proj, zx_proj, vmax, size, title=title)
 
 def render_xyz(xy_proj:np.ndarray, zy_proj:np.ndarray, zx_proj:np.ndarray, vmax=None, size=(5,5)):
     fig = plt.figure(figsize=size)
@@ -83,16 +109,16 @@ def render_xyz(xy_proj:np.ndarray, zy_proj:np.ndarray, zx_proj:np.ndarray, vmax=
     
 
 
-def render_xy_xz(xy_proj:np.ndarray, zx_proj:np.ndarray, vmax=None, size=(5,5)):
+def render_xy_xz(xy_proj:np.ndarray, zx_proj:np.ndarray, vmax=None, size=(5,5), title=None):
     fig = plt.figure(figsize=size)
-    
-    fig.add_subplot(121)
+    plt.title(title)
+    fig.add_subplot(211)
     plt.imshow(xy_proj, 
                vmax=vmax, 
                cmap='hot')
     plt.title('xy')
     plt.axis('off')
-    fig.add_subplot(122)
+    fig.add_subplot(212)
     plt.imshow(zx_proj, 
                origin='bottom', 
                vmax=vmax, 
@@ -101,7 +127,7 @@ def render_xy_xz(xy_proj:np.ndarray, zx_proj:np.ndarray, vmax=None, size=(5,5)):
     plt.axis('off')
     
     plt.tight_layout()
-    plt.show()
+    #plt.show()
     print('vmax = ', xy_proj.max())
     
 
