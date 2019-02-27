e5ec546574a046bfef12d41928109b6b96cb4cd9
aaristov <andrey.aristov@pasteur.fr>
2019-02-27 11:15:29 +0100

    add pandas import

--- pyhist/iotools.py
+++ pyhist/iotools.py
@@ -21,4 +21,16 @@ def import_zola(path):
     return np.loadtxt(path, delimiter=',', skiprows=1, dtype=[(i, v) for i,v in zip(dtype,dtype.values())])
 
 def import_csv(path):
-    return pd.read_csv(path)
+    '''Loads csv table with pandas'''
+    table = pd.read_csv(path)
+    return table
+
+def get_zyx_array(pd_table):
+    try:
+        expected = ['frame', 'x [nm]', 'y [nm]', 'z [nm]']
+        for v in expected:
+            assert v in pd_table.columns.values, f'{v} column is missing. Table must contain {expected} columns'
+    except AssertionError:
+        print(f'{v} column is missing. Table must contain {expected} columns')
+        return False 
+    return pd_table[expected[-1:0:-1]].values
